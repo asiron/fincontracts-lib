f4406a231c478824bcf0142b3c9fcbfb045b99ef
Maciej Żurad <maciej.zurad@gmail.com>
2017-04-23 15:58:36 +0200

    fincontract deployer now uses visitor pattern

--- src/fincontract-deployer.js
+++ src/fincontract-deployer.js
@@ -1,86 +1,25 @@
-const finc = require('./fincontract');
+const dd = require('./fincontract-description-deployer')
 const sender = require('./tx-sender');
+
 var log = require('minilog')('deploy');
 require('minilog').enable();
 
 export class Deployer {
   
   constructor(marketplace, web3) {
+    this.dd = new dd.DescriptionDeployer(marketplace, web3);
     this.sender = new sender.Sender(marketplace, web3);
     this.marketplace = marketplace;
-    this.web3 = web3;
   }
 
   deploy(description) {
-    return this.visit(description)
+    return this.dd.deployDescription(description)
       .then(descID => this.deployFincontract(descID));
   }
 
   issue(description, proposedOwner) {
     return this.deploy(description)
       .then(fctID => this.issueFincontract(fctID, proposedOwner));
-  }
-
-  visit(node) {
-    const that = this;
-    switch (node.constructor) {
-
-      case finc.FincIfNode: {
-        const left  = this.visit(node.children[0]);
-        const right = this.visit(node.children[1]);
-        return Promise.all([left, right]).then(ids => {
-          const args = [node.gatewayAddress].concat(ids);
-          return that.deployPrimitive('If', args);
-        });
-      }
-
-      case finc.FincAndNode: {
-        const left  = this.visit(node.children[0]);
-        const right = this.visit(node.children[1]);
-        return Promise.all([left, right]).then(ids =>
-          that.deployPrimitive('And', ids)
-        );
-      }
-
-      case finc.FincOrNode: {
-        const left  = this.visit(node.children[0]);
-        const right = this.visit(node.children[1]);
-        return Promise.all([left, right]).then(ids =>
-          that.deployPrimitive('Or', ids)
-        );
-      }
-
-      case finc.FincTimeboundNode:
-        return this.visit(node.children).then(primitiveId => {
-          const args = [node.lowerBound, node.upperBound, primitiveId];
-          return that.deployPrimitive('Timebound', args);
-        });
-
-      case finc.FincGiveNode:   
-        return this.visit(node.children).then(primitiveId =>
-          that.deployPrimitive('Give', [primitiveId])
-        );
-
-      case finc.FincScaleObsNode:   
-        return this.visit(node.children).then(primitiveId => {
-          const args = [node.gatewayAddress, primitiveId];
-          return that.deployPrimitive('ScaleObs', args);
-        });
-
-      case finc.FincScaleNode:
-        return this.visit(node.children).then(primitiveId => {
-          const args = [node.scale, primitiveId];
-          return that.deployPrimitive('Scale', args);
-        });
-
-      case finc.FincOneNode:
-        return this.deployPrimitive('One', [node.currency]);
-
-      case finc.FincZeroNode:
-        return this.deployPrimitive('Zero', []);
-
-      default: throw('Error: Unknown case during primitive deployment');
-    }
   }
 
   issueFincontract(fctID, proposedOwner) {
@@ -101,14 +40,6 @@ export class Deployer {
       log.info("Fincontract: " + fctID);
       log.info("Created for: " + owner);
       return fctID;
-    });
-  }
-
-  deployPrimitive(name, args) {
-    return this.sender.send(name, args, {event: 'PrimitiveStoredAt'}, (logs) => {
-      const primitiveId = logs.args.id;
-      log.info(name + ' primitive ID: ' + primitiveId);
-      return primitiveId;
     });
   }
 }
---
+++ src/fincontract-description-deployer.js
@@ -0,0 +1,87 @@
+const sender = require('./tx-sender');
+const v = require('./fincontract-visitor');
+
+var log = require('minilog')('desc-deploy');
+require('minilog').enable();
+
+export class DescriptionDeployer extends v.Visitor {
+  
+  constructor(marketplace, web3) {
+  	super();
+    this.marketplace = marketplace;
+  	this.sender = new sender.Sender(marketplace, web3);
+  }
+
+  deployDescription(node) {
+  	return this.visit(node);
+  }
+
+  deployPrimitive(name, args) {
+    return this.sender.send(name, args, {event: 'PrimitiveStoredAt'}, (logs) => {
+      const primitiveId = logs.args.id;
+      log.info(name + ' primitive ID: ' + primitiveId);
+      return primitiveId;
+    });
+  }
+
+  processAndNode(node, left, right) {
+    const that = this;
+    return Promise.all([left, right])
+      .then(ids => that.deployPrimitive('And', ids));
+  }
+
+  processIfNode(node, left, right) {
+    const that = this;
+    return Promise.all([left, right]).then(ids => {
+      const args = [node.gatewayAddress].concat(ids);
+      return that.deployPrimitive('If', args);
+    });
+  }
+
+  processOrNode(node, left, right) {
+    const that = this;
+    return Promise.all([left, right])
+      .then(ids => that.deployPrimitive('Or', ids));
+  }
+
+  processTimeboundNode(node, child) {
+    const that = this;
+    return child.then(primitiveId => {
+      const args = [node.lowerBound, node.upperBound, primitiveId];
+      return that.deployPrimitive('Timebound', args);
+    });
+  }
+
+  processGiveNode(node, child) {
+    const that = this;
+    return child.then(primitiveId => that.deployPrimitive('Give', [primitiveId]));
+  }
+    
+  processScaleObsNode(node, child) {
+    const that = this;
+   return child.then(primitiveId => {
+      const args = [node.gatewayAddress, primitiveId];
+      return that.deployPrimitive('ScaleObs', args);
+    });
+  }
+
+  processScaleNode(node, child) {
+    const that = this;
+    return child.then(primitiveId => {
+      const args = [node.scale, primitiveId];
+      return that.deployPrimitive('Scale', args);
+    });
+  }
+   
+  processOneNode(node) {
+    return this.deployPrimitive('One', [node.currency]);
+  }
+
+  processZeroNode(node) {
+    return this.deployPrimitive('Zero', []);
+  }
+
+  processUnknownNode(node) {
+    throw('Error: Unknown case during description deployment!');
+  }
+}
